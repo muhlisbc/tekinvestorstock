a4ffefd21db28f366f99d745fc336b416fffdaad
Jørgen Tronstad <jorgtron@gmail.com>
2015-04-11 22:36:56 +0200

    Ticker magic

--- assets/javascripts/alert.js
+++ assets/javascripts/alert.js
@@ -3,10 +3,12 @@
   setTimeout(function(){
 
       if($('li.current-user').length > 0) { loggedIn = true; } else { loggedIn = false;  }
+      
+      if(loggedIn){
+        addStockToUsersFavoriteStocks();
+        displayUsersFavoriteStocks();
+      }
 
-      addStockToUsersFavoriteStocks();
-      displayUsersFavoriteStocks();
-  
   }, 2000);
 
 
@@ -14,40 +16,39 @@
 
      if($('#stock_data').length == 0) { // stock data has not already been loaded
 
-        stocks = getUsersFavoriteStocks();
+        Discourse.ajax("/stock/get_users_favorite_stocks", {
+          type: "GET",
+        }).then(function(data) {
+        
+          console.log('users fav stocks: ');
+          console.log(data);
+        
+          stocks = getUsersFavoriteStocks();
+          console.log(stocks);
 
-        for (var i = stocks.length - 1; i >= 0; i--) {
-          console.log(stocks[i].symbol);
-        };
+          for (var i = stocks.length - 1; i >= 0; i--) {
+            console.log(stocks[i].symbol);
+          };
 
-        form_html = "<a href=''>Rediger din portefølje</a><div id='my_stock'><span class='label'>Antall aksjer:</span> <input id='num_stocks' type='text' placeholder='1000' /><span class='label'>Snittpris:</span> <input id='average_price' type='text' placeholder='5,3' /> </div> ";
+          form_html = "<a href=''>Rediger din portefølje</a><div id='my_stock'><span class='label'>Antall aksjer:</span> <input id='num_stocks' type='text' placeholder='1000' /><span class='label'>Snittpris:</span> <input id='average_price' type='text' placeholder='5,3' /> </div> ";
 
-        stock_html = 
-        '<div id="stock_data"><div class="container"><span id="stock_data_inner"><span class="stock_last"><span class="label">Kurs:</span> <strong>' + 0 + '</strong></span> <span class="stock_change_percent">(<strong>' + 0 + '</strong>%)</span> <span class="stock_my_total_value"><span class="label">Min aksjeverdi:</span> <strong>' + 0 + '</strong></span> <span class="value_change_today"><span class="label">I dag:</span> <span class="sign">+</span><strong>' + 0 + '</strong></span></span> <span class="value_change"><span><span class="label">Gevinst/tap:</span></span> <span class="sign">+</span><strong>' + 0 + '</strong></span> <span class="value_change_percent">(<span class="sign">+</span><strong>' + 0 + '</strong>%)</span>' + form_html + '</div></div>' ;
+          stock_html = 
+          '<div id="stock_data"><div class="container"><span id="stock_data_inner"><span class="stock_last"><span class="label">Kurs:</span> <strong>' + 0 + '</strong></span> <span class="stock_change_percent">(<strong>' + 0 + '</strong>%)</span> <span class="stock_my_total_value"><span class="label">Min aksjeverdi:</span> <strong>' + 0 + '</strong></span> <span class="value_change_today"><span class="label">I dag:</span> <span class="sign">+</span><strong>' + 0 + '</strong></span></span> <span class="value_change"><span><span class="label">Gevinst/tap:</span></span> <span class="sign">+</span><strong>' + 0 + '</strong></span> <span class="value_change_percent">(<span class="sign">+</span><strong>' + 0 + '</strong>%)</span>' + form_html + '</div></div>' ;
 
-        $('body').append(stock_html);
+          $('body').append(stock_html);
+
+        });
+
       }
 
   }
 
-  function getUsersFavoriteStocks() {
-    if(loggedIn){
-      Discourse.ajax("/stock/get_users_favorite_stocks", {
-        type: "GET",
-      }).then(function(data) {
-        console.log('users fav stocks: ');
-        console.log(data);
-        return data;
-      });
-    }
-  } 
-
   function addStockToUsersFavoriteStocks() {
-    if(loggedIn){
+    
         Discourse.ajax("/stock/add_stock_to_users_favorite_stocks?ticker=funcom.ol", {
         type: "GET",
       });
     }
-  } 
+  
 
 })();
