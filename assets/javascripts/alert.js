543eae45f62ef5cbec8207847ff19b26b9df8b81
Jørgen Tronstad <jorgtron@gmail.com>
2015-04-11 22:20:52 +0200

    Ticker magic

--- assets/javascripts/alert.js
+++ assets/javascripts/alert.js
@@ -4,10 +4,29 @@
 
       if($('li.current-user').length > 0) { loggedIn = true; } else { loggedIn = false;  }
 
-      getUsersFavoriteStocks();
       addStockToUsersFavoriteStocks();
   
   }, 2000);
+
+
+  function displayUsersFavoriteStocks() {
+
+     if($('#stock_data').length == 0) { // stock data has not already been loaded
+
+        stocks = getUsersFavoriteStocks();
+
+        for (var i = stocks.length - 1; i >= 0; i--) {
+          console.log(stocks[i].symbol);
+        };
+
+        form_html = "<a href=''>Rediger din portefølje</a><div id='my_stock'><span class='label'>Antall aksjer:</span> <input id='num_stocks' type='text' placeholder='1000' /><span class='label'>Snittpris:</span> <input id='average_price' type='text' placeholder='5,3' /> </div> ";
+
+        stock_html = 
+        '<div id="stock_data"><div class="container"><span id="stock_data_inner"><span class="stock_last"><span class="label">Kurs:</span> <strong>' + 0 + '</strong></span> <span class="stock_change_percent">(<strong>' + 0 + '</strong>%)</span> <span class="stock_my_total_value"><span class="label">Min aksjeverdi:</span> <strong>' + 0 + '</strong></span> <span class="value_change_today"><span class="label">I dag:</span> <span class="sign">+</span><strong>' + 0 + '</strong></span></span> <span class="value_change"><span><span class="label">Gevinst/tap:</span></span> <span class="sign">+</span><strong>' + 0 + '</strong></span> <span class="value_change_percent">(<span class="sign">+</span><strong>' + 0 + '</strong>%)</span>' + form_html + '</div></div>' ;
+
+        $('body').append(stock_html);
+
+  }
 
   function getUsersFavoriteStocks() {
     if(loggedIn){
@@ -16,6 +35,7 @@
       }).then(function(data) {
         console.log('users fav stocks: ');
         console.log(data);
+        return data;
       });
     }
   } 
