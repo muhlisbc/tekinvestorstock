f55066153f88962db9fd1e16e20e14c3e912278c
Jørgen Tronstad <jorgtron@gmail.com>
2015-04-16 23:09:44 +0200

    Ticker magic

--- assets/javascripts/alert.js
+++ assets/javascripts/alert.js
@@ -71,7 +71,11 @@
           };
 
           if($('#stock_data').length == 0 || forceRefresh) { // stock data has not already been loaded
-
+            
+            if(forceRefresh) {
+              $('#stock_data').remove();
+            }
+            
             stock_html = '<div id="stock_data"><div class="container"><div id="stock_data_inner">' + template + '</div></div></div>';
 
             $('body').append(stock_html);
