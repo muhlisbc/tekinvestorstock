405fedbd4fbe7dc182c0f0e5bb1be2aca75f744d
Jørgen Tronstad <jorgtron@gmail.com>
2015-04-16 22:31:38 +0200

    Ticker magic

--- assets/javascripts/alert.js
+++ assets/javascripts/alert.js
@@ -18,7 +18,7 @@
   }, 60000);
 
 
-  function displayUsersFavoriteStocks() {
+  function displayUsersFavoriteStocks(forceRefresh = false) {
 
         Discourse.ajax("/stock/get_users_favorite_stocks", {
           type: "GET",
@@ -64,13 +64,13 @@
 
             }
 
-            if($('#stock_data').length == 0) { // stock data has not already been loaded
+            if($('#stock_data').length == 0 || forceRefresh) { // stock data has not already been loaded
               template = template + '<a data-symbol="' + nameForUrl + '" href="/tags/' + nameForUrl.toLowerCase() + '"><div class="change_icon  ' + change_direction + '"><div></div></div><span class="stock_symbol">' + nameForUrl + '</span><div class="stock_extra"><span class="stock_last number-animate">' + last_trade_price_only + '</span><span class="stock_change_percent ' + change_direction + '"><span class="number-animate">' + percent_change + '</span>%</span></div></a>';
             }
 
           };
 
-          if($('#stock_data').length == 0) { // stock data has not already been loaded
+          if($('#stock_data').length == 0 || forceRefresh) { // stock data has not already been loaded
 
             stock_html = '<div id="stock_data"><div class="container"><div id="stock_data_inner">' + template + '</div></div></div>';
 
