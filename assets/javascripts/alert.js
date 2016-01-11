9aeec3c99f057b4f1147684c9bf1293c79c435fd
Jørgen Tronstad <jorgtron@gmail.com>
2016-01-11 12:36:25 +0100

    update after pull

--- assets/javascripts/alert.js
+++ assets/javascripts/alert.js
@@ -120,8 +120,7 @@
 
   }
 
-
-function displayTekIndex(forceRefresh) {
+  function displayTekIndex(forceRefresh) {
         console.log('displayTekIndex');
         console.log('forceRefresh:' + forceRefresh);
         $('#tekindex .notice-not-logged-in').hide();
@@ -137,65 +136,59 @@ function displayTekIndex(forceRefresh) {
 
           template = '';
           console.log(data.stock);
-          
-          if(data.stock.length > 0) {
-          
-            for (var i = data.stock.length - 1; i >= 0; i--) {
-              
-              //console.log('stock #' + i);
-              
-              //console.log(data.stock[i]);
-              stock = jQuery.parseJSON(data.stock[i]);
-              //console.log('stock:' + stock);
-              console.log('symbol:' + stock.symbol);
-              nameForUrl = stock.symbol;//.substring(0, stock.symbol.indexOf('.'));
-              //console.log(stock.last_trade_price_only);  
-  
-              if(stock.last_trade_price_only != null) {
-  
-                last_trade_price_only = stock.last_trade_price_only.toString().replace(".",",");
-  
-                percent_change = stock.percent_change.toString();
-                percent_change = percent_change.replace("%","");
-                percent_change = percent_change.replace(".",",");
-                
-                change_direction = 'neutral';
-                //console.log(percent_change);
-                if(percent_change.indexOf("-") != -1){ change_direction = 'negative'; }
-                if(percent_change.indexOf("+") != -1){ change_direction = 'positive'; }
-  
-                if($('#stock_data_tekindex').length > 0 && forceRefresh == false) { // stock data has been loaded, update existing stock numbers
-                  // update data
-                  console.log('updating ' + nameForUrl + ' data to: ' + last_trade_price_only + ', ' + percent_change + ' ' + nameForUrl + ', ' + last_trade_price_only);
-                  //console.log($('#stock_data a[data-symbol="' + nameForUrl + '"] .stock_last'));
-                  $('#stock_data_tekindex tr[data-symbol="' + nameForUrl + '"] .stock_last').numberAnimate('set', last_trade_price_only);
-                  $('#stock_data_tekindex tr[data-symbol="' + nameForUrl + '"] .stock_change_percent .number-animate').numberAnimate('set', percent_change);
-                  // set up or down
-                  $('#stock_data_tekindex tr[data-symbol="' + nameForUrl + '"] .change_icon, #stock_data tr[data-symbol="' + nameForUrl + '"] .stock_change_percent').removeClass('positive').removeClass('negative').addClass(change_direction);
-  
-                }
-  
-                if($('#stock_data_tekindex').length == 0 || forceRefresh) { // stock data has not already been loaded
-                  template = template + '<tr data-symbol="' + nameForUrl + '"><td class="td-ticker"><a href="/tags/' + nameForUrl.toLowerCase() + '"><span class="stock_symbol">' + nameForUrl + '</span></a></td><td class="td-last"><span class="stock_last number-animate">' + last_trade_price_only + '</span></td><td class="td-change"><span class="stock_change_percent ' + change_direction + '"><span class="number-animate">' + percent_change + '</span>%</span></td></tr>';
-                }
-                
-                $('#user-favorite-stocks .notice-no-favorites').hide();
-              }
-  
-            };
+        
+          for (var i = data.stock.length - 1; i >= 0; i--) {
             
-          } else {
-            $('#user-favorite-stocks .notice-no-favorites').show();
-          }
+            //console.log('stock #' + i);
+            
+            //console.log(data.stock[i]);
+            stock = jQuery.parseJSON(data.stock[i]);
+            //console.log('stock:' + stock);
+            console.log('symbol:' + stock.symbol);
+            nameForUrl = stock.symbol;//.substring(0, stock.symbol.indexOf('.'));
+            //console.log(stock.last_trade_price_only);  
 
-          if($('#stock_data_tekindex').length == 0 || forceRefresh) { // stock data has not already been loaded
+            if(stock.last_trade_price_only != null) {
+
+              last_trade_price_only = stock.last_trade_price_only.toString().replace(".",",");
+
+              percent_change = stock.percent_change.toString();
+              percent_change = percent_change.replace("%","");
+              percent_change = percent_change.replace(".",",");
+              
+              change_direction = 'neutral';
+              //console.log(percent_change);
+              if(percent_change.indexOf("-") != -1){ change_direction = 'negative'; }
+              if(percent_change.indexOf("+") != -1){ change_direction = 'positive'; }
+
+              if($('#tekindex_stock_data').length > 0 && forceRefresh == false) { // stock data has been loaded, update existing stock numbers
+                // update data
+                console.log('updating ' + nameForUrl + ' data to: ' + last_trade_price_only + ', ' + percent_change + ' ' + nameForUrl + ', ' + last_trade_price_only);
+                //console.log($('#stock_data a[data-symbol="' + nameForUrl + '"] .stock_last'));
+                $('#tekindex_stock_data tr[data-symbol="' + nameForUrl + '"] .stock_last').numberAnimate('set', last_trade_price_only);
+                $('#tekindex_stock_data tr[data-symbol="' + nameForUrl + '"] .stock_change_percent .number-animate').numberAnimate('set', percent_change);
+                // set up or down
+                $('#tekindex_stock_data tr[data-symbol="' + nameForUrl + '"] .change_icon, #stock_data tr[data-symbol="' + nameForUrl + '"] .stock_change_percent').removeClass('positive').removeClass('negative').addClass(change_direction);
+
+              }
+
+              if($('#tekindex_stock_data').length == 0 || forceRefresh) { // stock data has not already been loaded
+                template = template + '<tr data-symbol="' + nameForUrl + '"><td class="td-ticker"><a href="/tags/' + nameForUrl.toLowerCase() + '"><span class="stock_symbol">' + nameForUrl + '</span></a></td><td class="td-last"><span class="stock_last number-animate">' + last_trade_price_only + '</span></td><td class="td-change"><span class="stock_change_percent ' + change_direction + '"><span class="number-animate">' + percent_change + '</span>%</span></td></tr>';
+              }
+              
+              $('#tekindex .notice-no-favorites').hide();
+            }
+
+          };
+
+          if($('#tekindex_stock_data').length == 0 || forceRefresh) { // stock data has not already been loaded
             
             if(forceRefresh) {
-              $('#stock_data_tekindex').remove();
+              $('#tekindex_stock_data').remove();
               //console.log('removing stock data');
             }
 
-            stock_html = '<div id="stock_data_tekindex"><div class="container"><table id="stock_data_inner"><thead><th class="th-symbol">Ticker</th><th class="th-last">Siste</th><th class="th-change">I dag</th></thead><tbody>' + template + '</tbody></table></div></div>';
+            stock_html = '<div id="tekindex_stock_data"><div class="container"><table id="stock_data_inner"><thead><th class="th-symbol">Ticker</th><th class="th-last">Siste</th><th class="th-change">I dag</th></thead><tbody>' + template + '</tbody></table></div></div>';
             //console.log(stock_html);
             $('#tekindex .spinner').hide();
             $('#tekindex').append(stock_html);              
@@ -205,7 +198,6 @@ function displayTekIndex(forceRefresh) {
         });
 
   }
-
 
   function addStockToUsersFavoriteStocks(ticker) {
     
