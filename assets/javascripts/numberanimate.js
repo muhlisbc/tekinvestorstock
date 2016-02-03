8496d1435d7da42697e865c01ec4d3b18c591cde
Jørgen Tronstad <jorgtron@gmail.com>
2016-02-03 21:52:27 +0100

    typo

--- assets/javascripts/numberanimate.js
+++ assets/javascripts/numberanimate.js
@@ -200,7 +200,7 @@
                 var valueStr = this.innerHTML;
                 if (!checkValue(valueStr)) return;
 
-                $this.attr("data-numberAnimate-value", parseFloat(valueStr).toFixed(2);
+                $this.attr("data-numberAnimate-value", parseFloat(valueStr).toFixed(2));
                 
                 if (!animationSupported) return; //do nothing..
 
@@ -263,7 +263,7 @@
 
             //get the number value and update the stored value..
             if (!checkValue(newValue))  return;
-            this.attr("data-numberAnimate-value", parseFloat(newValue).toFixed(2);
+            this.attr("data-numberAnimate-value", parseFloat(newValue).toFixed(2));
 
             //if not animating just change the value..
             if (!animationSupported) {
