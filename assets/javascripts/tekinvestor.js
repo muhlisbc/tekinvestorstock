1e1ae2596119a5950c75efc4cbb2a589d6b1a844
Jørgen Tronstad <jorgtron@gmail.com>
2016-02-03 21:16:37 +0100

    last try

--- assets/javascripts/tekinvestor.js
+++ assets/javascripts/tekinvestor.js
@@ -297,14 +297,5 @@
     // formats 1 -> 1.00
     {
         //console.log('formatting number: ' + number);
-        var number = parseFloat(number);
-        number = number.toFixed(2) + '';
-        var x = number.split('.');
-        var x1 = x[0];
-        var x2 = x.length > 1 ? '.' + x[1] : '';
-        var rgx = /(\d+)(\d{3})/;
-        while (rgx.test(x1)) {
-            x1 = x1.replace(rgx, '$1' + ',' + '$2');
-        }
-        return x1 + x2;
-}
+        return parseFloat(number).toFixed(2);
+    }
