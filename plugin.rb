49cbdd7189712af1af2002c976c69472bf38928e
Jørgen Tronstad <jorgtron@gmail.com>
2017-08-29 23:13:46 +0200

    read rest-client gem since rebuild not working

--- plugin.rb
+++ plugin.rb
@@ -21,7 +21,7 @@ StockPlugin = StockPlugin
   #gem 'webmock', '~> 1.24.6'
   #gem 'rubocop', '~> 0.40'
   #gem 'byebug', '~> 9.0.5'
-  gem 'rest-client', '2.0.2'
+  gem 'rest-client', '1.8'
   gem 'json', '2.0.4'
   gem 'stock_quote', '1.1.2' ## comment this out on local, but should be there for prod. gems above are dependencies for this gem
 
