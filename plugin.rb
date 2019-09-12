037015d5ed2875ae19412a8b019a3713a47a0167
jorgtron <jorgtron@gmail.com>
2019-09-12 10:51:16 +0200

    add drip plugin

--- plugin.rb
+++ plugin.rb
@@ -10,6 +10,8 @@ register_asset "javascripts/charting_library/charting_library.min.js"
 
 require 'net/http'
 
+gem 'drip-ruby', '3.3.1', {require: false}
+
 load File.expand_path("../stock.rb", __FILE__)
 
 
