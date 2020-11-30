f863b6bac41e74cf88f56c8f1e6432bafc3b2b4a
jorgtron <jorgtron@gmail.com>
2020-12-01 00:23:32 +0100

    add batching for email export

--- app/jobs/scheduled/email_export.rb
+++ app/jobs/scheduled/email_export.rb
@@ -5,7 +5,7 @@ module Jobs
 
     # export email addyes to email sender (drip)
 
-    every 15.minutes
+    every 1.hour
 
     def execute(args)
         
@@ -13,74 +13,80 @@ module Jobs
           c.api_key = "eqwaxhhcryfcxxdyqwhu"
           c.account_id = "9675646"
         end
+        
+	group = Group.find_by("lower(name) = ?", "insider")
+	    
+	    Person.find_each(finish: 9_999) do |person|
+		  person.party_all_night!
+		end
 
-        subscribersListStart = '{"batches": [{"subscribers": ['
-        subscribersListEnd = '}]}]}'
 
         puts "Finding all users"
 
-        users = User.order(last_seen_at: :desc).limit(999) # Drip has 1000 element limit in batches
+        users_all = User.find_in_batches(batch_size: 1000) do |users| # Drip has 1000 element limit in batches
 
-        users.each do |user|
-	  	
-  		  	puts "processing:" + user.email
-          isInsider = false
+		subscribersListStart = '{"batches": [{"subscribers": ['
+		subscribersListEnd = '}]}]}'
 
-          group = Group.find_by("lower(name) = ?", "insider")
-          
-          if group && GroupUser.where(user_id: user.id, group_id: group.id).exists? 
-            isInsider = true
-          else
-            isInsider = false
-          end
-		
-	   # find stats for this user
-	   results = nil
-	   sql = "SELECT SUM(reads) as sum_reads, count(id) as post_count, avg(like_count) AS like_avg, sum(like_count) as total_likes_received, user_id from posts Where user_id = " + user.id.to_s + " group by user_id"
-	   results = ActiveRecord::Base.connection.execute(sql)
+		users.each do |user|
 
-		sum_reads = 0
-		post_count = 0
-		like_avg = 0
-		total_likes_received = 0
+		 # puts "processing:" + user.email
+		  isInsider = false
 
-		if !results.to_a.empty?
-			sum_reads = results[0]["sum_reads"]
-			post_count = results[0]["post_count"]
-			like_avg = results[0]["like_avg"]
-			total_likes_received = results[0]["total_likes_received"]
-			
-			puts sum_reads + " / " + post_count + " / " + like_avg + " / " + total_likes_received
-			
-		end
-  		  	
-          subscriberInfo = '{
-                "email": "' + user.email + '",
-                "time_zone": "Europe/Copenhagen",
-                "custom_fields": {
-                  "insider": "' + isInsider.to_s + '",
-                  "username": "' + user.username + '",
-                  "users_posts_read_x_times": "' + sum_reads.to_s + '",
-                  "post_count": "' + post_count.to_s + '",
-                  "likes_received_per_post": "' + like_avg.to_s + '",
-                  "total_likes_received": "' + total_likes_received.to_s + '",
-                  "created_at": "' + user.created_at.to_s + '",
-                  "last_seen_at": "' + user.last_seen_at.to_s + '"
-                }
-          },'
+		  if group && GroupUser.where(user_id: user.id, group_id: group.id).exists? 
+		    isInsider = true
+		  else
+		    isInsider = false
+		  end
 
-	  puts subscriberInfo
-		
-          subscribersListStart = subscribersListStart + subscriberInfo
+		   # find stats for this user
+		   results = nil
+		   sql = "SELECT SUM(reads) as sum_reads, count(id) as post_count, avg(like_count) AS like_avg, sum(like_count) as total_likes_received, user_id from posts Where user_id = " + user.id.to_s + " group by user_id"
+		   results = ActiveRecord::Base.connection.execute(sql)
 
-  		  end
+			sum_reads = 0
+			post_count = 0
+			like_avg = 0
+			total_likes_received = 0
 
-        subscribers = subscribersListStart.slice!(subscribersListStart.length-1,subscribersListStart.length) + subscribersListEnd #remove trailing ,
-        puts subscribers
+			if !results.to_a.empty?
+				sum_reads = results[0]["sum_reads"]
+				post_count = results[0]["post_count"]
+				like_avg = results[0]["like_avg"]
+				total_likes_received = results[0]["total_likes_received"]
 
-        resp = client.create_or_update_subscribers(subscribers)
-        puts resp
+				puts sum_reads + " / " + post_count + " / " + like_avg + " / " + total_likes_received
 
+			end
+
+		  subscriberInfo = '{
+			"email": "' + user.email + '",
+			"time_zone": "Europe/Copenhagen",
+			"custom_fields": {
+			  "insider": "' + isInsider.to_s + '",
+			  "username": "' + user.username + '",
+			  "users_posts_read_x_times": "' + sum_reads.to_s + '",
+			  "post_count": "' + post_count.to_s + '",
+			  "likes_received_per_post": "' + like_avg.to_s + '",
+			  "total_likes_received": "' + total_likes_received.to_s + '",
+			  "created_at": "' + user.created_at.to_s + '",
+			  "last_seen_at": "' + user.last_seen_at.to_s + '"
+			}
+		  },'
+
+		  puts subscriberInfo
+
+		  subscribersListStart = subscribersListStart + subscriberInfo
+
+			  end
+
+		subscribers = subscribersListStart.slice!(subscribersListStart.length-1,subscribersListStart.length) + subscribersListEnd #remove trailing ,
+		puts subscribers
+
+		resp = client.create_or_update_subscribers(subscribers)
+		puts resp
+	end
+	    
         puts "import complete"
 
       end
