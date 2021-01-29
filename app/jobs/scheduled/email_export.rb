c77b808bb9ca7000ddb4ac81ec39943276815c80
jorgtron <jorgtron@gmail.com>
2021-01-29 11:07:03 +0100

    more stats in drip

--- app/jobs/scheduled/email_export.rb
+++ app/jobs/scheduled/email_export.rb
@@ -66,20 +66,54 @@ module Jobs
 
 				end
 
+			   # find more stats for this user
+			   results2 = nil
+			   sql2 = "SELECT * from user_stats Where user_id = " + user.id.to_s
+			   results2 = ActiveRecord::Base.connection.execute(sql2)
+
+			days_visited = 0
+			topics_entered = 0
+			time_read = 0
+			posts_read_count = 0
+			likes_given = 0
+			topic_count = 0
+			bounce_score = 0
+
+			if !results2.to_a.empty?
+				days_visited = results2[0]["days_visited"]
+				topics_entered = results2[0]["topics_entered"]
+				time_read = results2[0]["time_read"]
+				posts_read_count = results2[0]["posts_read_count"]
+				likes_given = results2[0]["likes_given"]
+				topic_count = results2[0]["topic_count"]
+				bounce_score = results2[0]["bounce_score"]
+				first_post_created_at = results2[0]["first_post_created_at"]
+			end
+			
 			  subscriber_hash = {
 				"email":  user_email,
+				"active": user.active,
+				"user_id": user.id, 
 				"time_zone": "Europe/Copenhagen",
 				"custom_fields": {
-				  "insider": isInsider.to_s,
-				  "username": user.username,
-				  "users_posts_read_x_times": sum_reads.to_s,
-				  "post_count": post_count.to_s,
-				  "likes_received_per_post": like_avg.to_s,
-				  "total_likes_received": total_likes_received.to_s,
-				  "created_at": user.created_at.to_s,
-				  "last_seen_at": user.last_seen_at.to_s
-					}
-			 	 }
+				"insider": isInsider.to_s,
+				"username": user.username,
+				"days_visited": days_visited,
+				"topics_entered": topics_entered,
+				"time_read": time_read,
+				"posts_read_count": posts_read_count,
+				"likes_given": likes_given,
+				"topic_count": topic_count,
+				"bounce_score": bounce_score,
+				"users_posts_read_x_times": sum_reads.to_s,
+				"post_count": post_count.to_s,
+				"likes_received_per_post": like_avg.to_s,
+				"total_likes_received": total_likes_received.to_s,
+				"created_at": user.created_at.to_s,
+			        "first_post_created_at": first_post_created_at.to_s,
+				"last_seen_at": user.last_seen_at.to_s
+			     }
+			 }
 				
 			# An array of subscribers
 			subscribers_array = subscribers_array.push(subscriber_hash)
